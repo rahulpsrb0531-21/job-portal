fad3e95014d8ee2c70d78a7fe60ee288bb7f0014
RAKESH-TAMBOLI <rakeshtamboli006688@gmail.com>
2024-03-03 12:18:23 +0530

    change server js file ip address

--- frontend/src/utils/server.js
+++ frontend/src/utils/server.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export const server = axios.create({
-    // baseURL: 'http://192.168.0.102:5000/',
-    baseURL: 'http://localhost:5000/',
+    baseURL: 'http://212.71.239.179:5000',
+    // baseURL: 'http://localhost:5000/',
     responseType: 'json',
     headers: {
         // "Access-Control-Allow-Origin": "*",
